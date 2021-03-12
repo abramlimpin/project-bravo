c8320a541e8b54434353f6be2080afc3830c5b09
Abram John Limpin <limpin.abram@gmail.com>
2021-03-12 16:03:16 +0800

    Added initial files

---
+++ MyInventory/Migrations/20210205075451_AddContactNoToSuppliers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyInventory.Migrations
+{
+    public partial class AddContactNoToSuppliers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactNo",
+                table: "Suppliers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactNo",
+                table: "Suppliers");
+        }
+    }
+}
